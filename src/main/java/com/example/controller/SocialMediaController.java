1ff2cf2fbb5900f5147e52ac244caab4f60025ff
Matthew Bixby <matthew.bixby1@gmail.com>
2025-05-02 18:09:11 +0000

    small change

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -20,7 +20,7 @@ import com.example.service.AccountService;
 import com.example.service.MessageService;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
+ * DONE / TESTS passed 38/28:  You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
  * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
